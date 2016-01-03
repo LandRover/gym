c1d0141cb9d30bd3df79938c6c8b2a567c6a81ca
LandRover <github.com@landroverz.com>
2016-01-03 11:49:37 +0000

    add, remove and find queen

--- eight-queens-ruby/src/game/board.rb
+++ eight-queens-ruby/src/game/board.rb
@@ -29,8 +29,33 @@ class Board
     
     
     def rows_end
-        rows_total -1
+        rows_total - 1
     end
     
     
+    def queen_add(x=0, y=0)
+        queen = Queen.new
+        queen.x = x
+        queen.y = y
+        
+        @queens << queen
+        
+        return queen
+    end
+    
+    
+    def queen_remove(x, y)
+        queen = queen_find(x, y)
+        
+        if queen
+            @queens.delete(queen)
+        end
+    end
+    
+    
+    private
+        
+        def queen_find(x, y)
+            @queens.detect {|q| q.location?(x, y)}
+        end
 end
