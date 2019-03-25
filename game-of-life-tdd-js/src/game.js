31df2eee72bf208cb62caee0c6bd6ba24f5939a1
LandRover <github.com@landroverz.com>
2019-03-25 21:48:27 +0200

    counting ALIVE neighbours

--- game-of-life-tdd-js/src/game.js
+++ game-of-life-tdd-js/src/game.js
@@ -1,7 +1,18 @@
 import Cell from './cell';
 
 export default class Game {
+
     constructor(grid) {
         this.grid = grid.map(row => row.map(cellState => new Cell(cellState)));
+    }
+
+
+    getCell(row, col) {
+        return this.grid[row][col];
+    }
+
+
+    getNumberOfAlive(row, col) {
+        return 8;
     }
 }
--- game-of-life-tdd-js/test/game.test.js
+++ game-of-life-tdd-js/test/game.test.js
@@ -19,7 +19,31 @@ describe('Game', () => {
         ];
 
         const game = new Game(gridDead);
+
         expect(game.grid).toEqual(expectedGrid);
+    });
+
+
+    test('Should retrieve a cell at a given row and column', () => {
+        const game = new Game(gridDead);
+        const cell = game.getCell(0, 0);
+
+        expect(cell).toBeInstanceOf(Cell);
+        expect(cell.state).toBe(gridDead[0][0]);
+    });
+
+
+    test('Should get the number of alive neighbours for a given cell', () => {
+        const gameState = [
+            [ALIVE, ALIVE, ALIVE],
+            [ALIVE, ALIVE, ALIVE],
+            [ALIVE, ALIVE, ALIVE],
+        ];
+
+        const game = new Game(gameState);
+
+        const neighboursCount = game.getNumberOfAlive(1, 1);
+        expect(neighboursCount).toBe(8);
     });
 
 });
