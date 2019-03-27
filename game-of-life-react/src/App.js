4635dab78d71d96af6892ecb00fb8dd94cc690cd
LandRover <github.com@landroverz.com>
2019-03-27 08:13:57 +0200

    initial react init with game of life loaded

---
+++ game-of-life-react/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import GameOfLife from '../../game-of-life-tdd-js/';
+import './App.css';
+
+const { CellState, Game } = GameOfLife;
+const { DEAD, ALIVE } = CellState;
+
+const initialGrid = [
+  [DEAD, DEAD, DEAD, DEAD, DEAD],
+  [DEAD, DEAD, ALIVE, DEAD, DEAD],
+  [DEAD, DEAD, ALIVE, DEAD, DEAD],
+  [DEAD, DEAD, ALIVE, DEAD, DEAD],
+  [DEAD, DEAD, DEAD, DEAD, DEAD],
+];
+
+const game = new Game(initialGrid);
+
+class App extends Component {
+  state = {
+    grid: game.grid
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Conway's Game of Life</h1>
+
+        <table>
+          {
+            this.state.grid.map((row, rowNumber) => (
+              <tr key={rowNumber}>
+                {
+                  row.map((cell, colNumber) => (
+                    <td key={colNumber} className="cell">x</td>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
